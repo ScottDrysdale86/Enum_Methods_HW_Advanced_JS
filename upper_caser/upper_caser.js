b705a9357261818e8e13feebbd99d66353ed4ab9
ScottDrysdale86 <sdrysdale86@pm.me>
2022-07-27 13:24:58 +0100

    uppercase complete

--- upper_caser/upper_caser.js
+++ upper_caser/upper_caser.js
@@ -1,8 +1,9 @@
 const UpperCaser = function (words) {
-}
+	this.words = words;
+};
 
 UpperCaser.prototype.toUpperCase = function () {
-
-}
+	return this.words.map((words) => words.toUpperCase());
+};
 
 module.exports = UpperCaser;
