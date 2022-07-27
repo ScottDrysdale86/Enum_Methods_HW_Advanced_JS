11892133d6f6d0b2f2afc81c3d7479fc5a9026be
ScottDrysdale86 <sdrysdale86@pm.me>
2022-07-27 20:22:57 +0100

    pangram using every and includes

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,22 +1,34 @@
 const PangramFinder = function (phrase) {
+	// this.alphabet = "qwertyuiopasdfghjklzxcvbnm".split("");
+	// this.phrase = phrase
+	// 	.toLowerCase()
+	// 	.replace(/[^a-z]/g, "")
+	// 	.split("");
+	// this.symbols = " !_;:?".split("");
+
 	this.alphabet = "qwertyuiopasdfghjklzxcvbnm".split("");
-	this.phrase = phrase
-		.toLowerCase()
-		.replace(/[^a-z]/g, "")
-		.split("");
-	this.symbols = " !_;:?".split("");
+	this.phrase = phrase;
 };
 
 PangramFinder.prototype.isPangram = function () {
-	const sortedPhrase = this.phrase.sort();
-	const uniqPhrase = [...new Set(sortedPhrase)];
-	const sortedAz = this.alphabet.sort();
+	// const sortedPhrase = this.phrase.sort();
+	// const uniqPhrase = [...new Set(sortedPhrase)];
+	// const sortedAz = this.alphabet.sort();
 
-	let isEqual =
-		uniqPhrase.length === sortedAz.length &&
-		uniqPhrase.every((letter, ind) => letter === sortedAz[ind]);
+	// let isEqual =
+	// 	uniqPhrase.length === sortedAz.length &&
+	// 	uniqPhrase.every((letter, ind) => letter === sortedAz[ind]);
 
-	return isEqual;
+	// return isEqual;
+
+	let phraseLower = this.phrase.toLowerCase();
+
+	return this.alphabet.every((letter) => {
+		if (!phraseLower.includes(letter)) {
+			return false;
+		}
+		return true;
+	});
 };
 
 module.exports = PangramFinder;
