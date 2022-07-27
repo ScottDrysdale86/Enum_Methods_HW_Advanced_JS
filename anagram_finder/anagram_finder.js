5df8dc2ec0f0ec1de43bd1b7c8a712f369e0e2dc
ScottDrysdale86 <sdrysdale86@pm.me>
2022-07-27 16:04:00 +0100

    finished anagram

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,19 @@
 const AnagramFinder = function (word) {
-
-}
+	this.word = word;
+	this.sortedWord = word.toLowerCase().split("").sort().join("");
+};
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
-}
+	let result = [];
+	otherWords.forEach((word) => {
+		if (
+			this.word !== word &&
+			word.toLowerCase().split("").sort().join("") === this.sortedWord
+		) {
+			result.push(word);
+		}
+	});
+	return result;
+};
 
 module.exports = AnagramFinder;
