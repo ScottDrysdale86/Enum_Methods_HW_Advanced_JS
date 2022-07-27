93796497c5687cda1a3f00e9a9800b2765fd720c
ScottDrysdale86 <sdrysdale86@pm.me>
2022-07-27 15:15:02 +0100

    pangram completed

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,22 @@
 const PangramFinder = function (phrase) {
-  this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
-}
+	this.alphabet = "qwertyuiopasdfghjklzxcvbnm".split("");
+	this.phrase = phrase
+		.toLowerCase()
+		.replace(/[^a-z]/g, "")
+		.split("");
+	this.symbols = " !_;:?".split("");
+};
 
 PangramFinder.prototype.isPangram = function () {
+	const sortedPhrase = this.phrase.sort();
+	const uniqPhrase = [...new Set(sortedPhrase)];
+	const sortedAz = this.alphabet.sort();
 
-}
+	let isEqual =
+		uniqPhrase.length === sortedAz.length &&
+		uniqPhrase.every((letter, ind) => letter === sortedAz[ind]);
+
+	return isEqual;
+};
 
 module.exports = PangramFinder;
