04b949fe43cd52772cc6b17afdb6997905815455
ScottDrysdale86 <sdrysdale86@pm.me>
2022-07-27 20:02:53 +0100

    use of .every for isogram added

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,10 +1,14 @@
 const IsogramFinder = function (word) {
-	this.word = word.split("");
+	// this.word = word.split("");
+	this.word = word;
 };
 
 IsogramFinder.prototype.isIsogram = function () {
-	const uniqueWord = [...new Set(this.word)];
-	return this.word.length === uniqueWord.length;
+	// const uniqueWord = [...new Set(this.word)];
+	// return this.word.length === uniqueWord.length;
+	return this.word
+		.split("")
+		.every((letter, ind) => this.word.indexOf(letter) == ind);
 };
 
 module.exports = IsogramFinder;
