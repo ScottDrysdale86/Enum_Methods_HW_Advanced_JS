16949fdb485cc3e59e81b6bbe1f5d2a1c8b48441
ScottDrysdale86 <sdrysdale86@pm.me>
2022-07-27 15:34:37 +0100

    isogram completed

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,10 @@
 const IsogramFinder = function (word) {
-
-}
+	this.word = word.split("");
+};
 
 IsogramFinder.prototype.isIsogram = function () {
-
-}
+	const uniqueWord = [...new Set(this.word)];
+	return this.word.length === uniqueWord.length;
+};
 
 module.exports = IsogramFinder;
